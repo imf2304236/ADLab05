e2e71e69f6a7441fd2715cc783814f4fb1442478
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-12-04 15:01:00 +0100

    Initialize BST "root" Node field and Node class

--- src/BST.java
+++ src/BST.java
@@ -2,5 +2,21 @@
  * ADLab05
  * Created by IF on 04.12.17.
  */
-public class BST {
+public class BST <Key extends Comparable<Key>, Value>
+{
+    private Node root;
+
+    private class Node
+    {
+        private Key key;
+        private Value value;
+        private Node left, right;
+        private int N;
+
+        public Node(Key key, Value value, int N) {
+            this.key = key;
+            this.value = value;
+            this.N = N;
+        }
+    }
 }
