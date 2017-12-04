ef0ec4342fdd2042500f135779e8b92b26116f0a
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-12-04 16:25:17 +0100

    Revise BST class to follow implementation from slides

--- src/BST.java
+++ src/BST.java
@@ -6,59 +6,44 @@ public class BST <Key extends Comparable<Key>, Value>
 {
     private Node root;
 
-    /**
-     *
-     */
     private class Node
     {
         private Key key;
         private Value val;
         private Node left, right;
-        private int N;
+//        private int N;
 
-        public Node(Key key, Value val, int N) {
+//        public Node(Key key, Value val, int N) {
+        public Node(Key key, Value val) {
             this.key = key;
             this.val = val;
-            this.N = N;
+//            this.N = N;
         }
     }
 
-    /**
-     *
-     * @return
-     */
+/*
     public int size()
     {
         return size(root);
     }
+*/
 
-    /**
-     *
-     * @param x
-     * @return
-     */
+/*
     private int size(Node x)
     {
         if (x == null) return 0;
         else           return x.N;
     }
+*/
 
-    /**
-     *
-     * @param key
-     * @return
-     */
+/*
     public Value get(Key key)
     {
         return get(root, key);
     }
+*/
 
-    /**
-     *
-     * @param x
-     * @param key
-     * @return
-     */
+/*
     private Value get(Node x, Key key)
     {
         if (x == null) return null;
@@ -68,6 +53,22 @@ public class BST <Key extends Comparable<Key>, Value>
         if      (cmp < 0) return get(x.left, key);
         else if (cmp > 0) return get(x.right, key);
         else              return x.val;
+    }
+*/
+
+    private Value get(Key key)
+    {
+        Node x = root;
+
+        while(x != null)
+        {
+            int cmp = key.compareTo(x.key);
+
+            if (cmp == 0)     return x.val;
+            else if (cmp < 0) x = x.left;
+            else              x = x.right;
+        }
+        return null;
     }
 
     /**
@@ -80,13 +81,7 @@ public class BST <Key extends Comparable<Key>, Value>
         root = put(root, key, val);
     }
 
-    /**
-     *
-     * @param x
-     * @param key
-     * @param val
-     * @return
-     */
+/*
     private Node put(Node x, Key key, Value val)
     {
         if (x == null) return new Node(key, val, 1);
@@ -98,6 +93,27 @@ public class BST <Key extends Comparable<Key>, Value>
         else              x.val = val;
 
         x.N = size(x.left) + size(x.right) + 1;
+
+        return x;
+    }
+*/
+
+    /**
+     *
+     * @param x
+     * @param key
+     * @param val
+     * @return
+     */
+    private Node put(Node x, Key key, Value val)
+    {
+        if (x == null) return new Node(key, val);
+
+        int cmp = key.compareTo(x.key);
+
+        if      (cmp < 0) x.left  = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else              x.val = val;
 
         return x;
     }
