b15445d0f29e141fab02ff7333c28e9edf009103
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-12-04 15:14:34 +0100

    Initialize BST.put() method definitions

--- src/BST.java
+++ src/BST.java
@@ -69,4 +69,36 @@ public class BST <Key extends Comparable<Key>, Value>
         else if (cmp > 0) return get(x.right, key);
         else              return x.val;
     }
+
+    /**
+     *
+     * @param key
+     * @param val
+     */
+    public void put(Key key, Value val)
+    {
+        root = put(root, key, val);
+    }
+
+    /**
+     *
+     * @param x
+     * @param key
+     * @param val
+     * @return
+     */
+    private Node put(Node x, Key key, Value val)
+    {
+        if (x == null) return new Node(key, val, 1);
+
+        int cmp = key.compareTo(x.key);
+
+        if      (cmp < 0) x.left  = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else              x.val = val;
+
+        x.N = size(x.left) + size(x.right) + 1;
+
+        return x;
+    }
 }
